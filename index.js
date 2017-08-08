139b11640e2e3df973c03ef32c824df3dc59c9b2
jen-harris <jennifer.louise.harris@gmail.com>
2017-08-08 21:18:40 +0100

    completing validation js file

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('hello');
+
+var errorMessage = document.getElementById('js-errorMessage');
+var submit = document.getElementById('submit');
+
+function emailCheck(){
+  var email = document.getElementById('email').value;
+  var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email===''){
+    var noEmail = document.createTextNode("Please enter your email");
+    return errorMessage.appendChild(noEmail);
+  } else if (!regex.test(email)){
+      var wrongEmail = document.createTextNode("Your email is invalid, please try again");
+      return errorMessage.appendChild(wrongEmail);
+    }
+}
+
+function passwordCheck() {
+  var password = document.getElementById('password').value;
+  var confirmPassword = document.getElementById('confirmPassword').value;
+  var email = document.getElementById('email').value;
+    if (password === '' || confirmPassword === '') {
+      if (email===''){
+      var br = document.createElement('br');
+      errorMessage.appendChild(br);
+      var emptyPassword = document.createTextNode("Please enter your password");
+      errorMessage.appendChild(emptyPassword)
+    } else {
+      var emptyPassword = document.createTextNode("Please enter your password");
+      errorMessage.appendChild(emptyPassword)
+      }
+    } else if (password !== confirmPassword) {
+      var passwordMatchError = document.createTextNode("Your passwords don't match. Please try again");
+      errorMessage.appendChild(passwordMatchError);
+    }
+}
+
+
+submit.addEventListener('click', function(e) {
+  e.preventDefault();
+  while(errorMessage.firstChild){
+    errorMessage.removeChild(errorMessage.firstChild);
+  }
+  emailCheck();
+  passwordCheck();
+})
